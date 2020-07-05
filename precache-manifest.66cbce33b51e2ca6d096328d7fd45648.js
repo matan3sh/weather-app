13e4b839514398c375befed5ed5cb17e01a736c3
Matan Shaviro <matanshaviro@matans-mbp-2.mynet>
2020-07-05 19:29:34 +0300

    Updates

---
+++ precache-manifest.66cbce33b51e2ca6d096328d7fd45648.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e8eff9cc21bcbc866555b864410cb72",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "679fad88218717c6ed19",
+    "url": "/weather-app/static/css/main.51f9030d.chunk.css"
+  },
+  {
+    "revision": "7a5d32e58b4603031747",
+    "url": "/weather-app/static/js/2.af5063d2.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/weather-app/static/js/2.af5063d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "679fad88218717c6ed19",
+    "url": "/weather-app/static/js/main.384667f9.chunk.js"
+  },
+  {
+    "revision": "1fdbaf6b4a98831695e6",
+    "url": "/weather-app/static/js/runtime-main.9e7ce6e3.js"
+  },
+  {
+    "revision": "3cf462b14aa57483498ee08d01ff4e28",
+    "url": "/weather-app/static/media/bg.3cf462b1.jpg"
+  }
+]);
